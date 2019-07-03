f87ede5204af8ed099fbf40f68270219e142a334
Adam Yala <adamyala@csail.mit.edu>
2019-07-03 13:56:58 -0400

    add support for mirai

--- config.py
+++ config.py
@@ -260,3 +260,36 @@ class MammoCancer1YrRiskImgOnlyConfig(Config):
     }
     ONCONET_ARGS = Args(ONCONET_CONFIG)
     ONCONET_ARGS = Args(ONCONET_CONFIG)
+
+class MammoCancerMirai(Config):
+    NAME = '2D_Mammo_Cancer_Mirai'
+    AGGREGATION="max"
+
+    ONCONET_CONFIG = {
+        'cuda': False,
+        'img_mean': [7240.058],
+        'img_std': [12072.904],
+        'img_size': [1664,2048],
+        'num_chan': 3,
+        'num_gpus': 1,
+        'test_image_transformers': ['scale_2d', 'align_to_left'],
+        'test_tensor_transformers': ["force_num_chan_2d", "normalize_2d"],
+        'additional': None,
+        'snapshot': 'snapshots/mgh_mammo_cancer_survival_risk_5yr_mirai_full_jul02.p',
+        'video':False,
+        "pred_risk_factors": True,
+        'use_pred_risk_factors_at_test': True,
+        'pred_both_sides': True,
+        'multi_image': True,
+        'num_images': 4,
+        'min_num_images': 4,
+        'model_name': 'mirai_full',
+        'max_followup': 5,
+        'use_risk_factors': True,
+        "use_region_annotation": False,
+        'risk_factor_keys': "density binary_family_history binary_biopsy_benign binary_biopsy_LCIS binary_biopsy_atypical_hyperplasia age menarche_age menopause_age first_pregnancy_age prior_hist race parous menopausal_status weight height ovarian_cancer ovarian_cancer_age ashkenazi brca mom_bc_cancer_history m_aunt_bc_cancer_history p_aunt_bc_cancer_history m_grandmother_bc_cancer_history p_grantmother_bc_cancer_history sister_bc_cancer_history mom_oc_cancer_history m_aunt_oc_cancer_history p_aunt_oc_cancer_history m_grandmother_oc_cancer_history p_grantmother_oc_cancer_history sister_oc_cancer_history hrt_type hrt_duration hrt_years_ago_stopped",
+        'use_second_order_risk_factor_features': False,
+        'callibrator_path': 'snapshot/callibrators/MIRAI_POST_NO_RF.callibrator.p'
+    }
+    ONCONET_ARGS = Args(ONCONET_CONFIG)
+    ONCONET_ARGS = Args(ONCONET_CONFIG)
--- oncoserve/onconet_wrapper.py
+++ oncoserve/onconet_wrapper.py
@@ -38,14 +38,14 @@ class OncoNetWrapper(object):
         # Unpack models taht were trained as data parallel
         if isinstance(self.model, nn.DataParallel):
             self.model = self.model.module
-        # Add use precomputed hiddens for models trained before it was introduced. 
+        # Add use precomputed hiddens for models trained before it was introduced.
         # Assumes a resnet base backbone
         try:
             self.model._model.args.use_precomputed_hiddens = args.use_precomputed_hiddens
             self.model._model.args.cuda = args.cuda
         except Exception as e:
             pass
-        # Load callibrator if desired 
+        # Load callibrator if desired
         if args.callibrator_path is not None:
             self.callibrator = pickle.load(open(args.callibrator_path,'rb'))
         else:
@@ -57,10 +57,10 @@ class OncoNetWrapper(object):
         self.logger = logger
 
 
-    def process_image(self, image, risk_factor_vector=None):
+    def process_image_indep(self, batch, risk_factor_vector=None):
         try:
             ## Apply transformers
-            x = self.transformer(image, self.args.additional)
+            x = self.transformer(batch['x'], self.args.additional)
             x = autograd.Variable(x.unsqueeze(0))
             risk_factors = autograd.Variable(risk_factor_vector.unsqueeze(0)) if risk_factor_vector is not None else None
             self.logger.info(IMG_START_CLASSIF_MESSAGE.format(x.size()))
@@ -80,12 +80,55 @@ class OncoNetWrapper(object):
             err_msg = ERR_MSG.format(e)
             raise Exception(err_msg)
 
+    def process_image_joint(self, batch, risk_factor_vector=None):
+        try:
+            ## Apply transformers
+            pdb.set_trace()
+            x = batch['x']
+            risk_factors = autograd.Variable(risk_factor_vector.unsqueeze(0)) if risk_factor_vector is not None else None
+            self.logger.info(IMG_START_CLASSIF_MESSAGE.format(x.size()))
+            if self.args.cuda:
+                x = x.cuda()
+                self.model = self.model.cuda()
+            else:
+                self.model = self.model.cpu()
+            ## Index 0 to toss batch dimension
+            logit, _, _ = self.model(x, risk_factors, batch)
+            if self.args.pred_both_sides:
+                logit, _ = torch.max( torch.cat( [logit['l'].unsqueeze(-1), logit['r'].unsqueeze(-1)], dim=-1), dim=-1)
+            probs = F.sigmoid(logit).cpu().data.numpy()
+            pred_y= np.zeros(probs.shape)
+            if self.callibrator is not None:
+                for i in self.callibrator.keys():
+                    pred_y[i] = self.callibrator[i].predict_proba(prob[i].reshape(-1,1))[0,1]
+            self.logger.info(IMG_FINISH_CLASSIF_MESSAGE.format(pred_y))
+            return pred_y
+        except Exception as e:
+            err_msg = ERR_MSG.format(e)
+            raise Exception(err_msg)
+
+
+
     def process_exam(self, images, risk_factor_vector):
         preds = []
-        for im in images:
-            preds.append(self.process_image(im, risk_factor_vector))
-        y = self.aggregator(preds)
-        if isinstance(y, np.generic):
-            y = y.item()
+        if self.args.model_name == 'mirai_full':
+            batch = self.collate_batch(images)
+            y = self.process_image_joint(images, risk_factor_vector)
+        else:
+            for im in images:
+                preds.append(self.process_image_indep(im, risk_factor_vector))
+            y = self.aggregator(preds)
+            if isinstance(y, np.generic):
+                y = y.item()
         self.logger.info(EXAM_CLASSIF_MESSAGE)
         return y
+
+    def collate_batch(self, images):
+        pdb.set_trace()
+        assert len(images) >= self.args.min_num_images
+        batch = {}
+        batch['side_seq'] = torch.cat([b['side_seq'] for b in images], dim=0)
+        batch['view_seq'] = torch.cat([b['view_seq'] for b in images], dim=0)
+        batch['x'] = torch.cat([ self.transformer(b['x'], self.args.additional) for b in images], dim=0)
+        return batch
+
